b1fb0a68c42036060c31c8fb24b1567c09d70bf9
otmane <aroussiotmane27@gmail.com>
2023-12-06 00:46:56 +0100

    ptf.c

--- ptf.c
+++ ptf.c
@@ -16,7 +16,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	if (format == NULL && format[1] == ' ' && !format[2])
+	if (format == NULL && format[0] == '\0')
 		return (-1);
 	while (*format)
 	{
