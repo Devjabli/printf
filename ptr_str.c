05110ed1e40e3bd5e9dbe909488d88bea86660e7
otmane <aroussiotmane27@gmail.com>
2023-12-07 00:03:45 +0100

    correct str

--- ptr_str.c
+++ ptr_str.c
@@ -10,9 +10,10 @@ void print_str(va_list args, int *cnt)
 {
 	char *str = va_arg(args, char *);
 
-	for (int i = 0; str[i] != '\0'; i++)
+	while (*str)
 	{
-		putchar(str[i]);
+		putchar(*str);
+		str++;
 		(*cnt)++;
 	}
 }
